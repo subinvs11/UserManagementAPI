a99c7288994ee3229825156c7a89f72eaa8f6880
subin <subinvs11@gmail.com>
2019-02-17 17:07:19 +0530

    login api completed

--- user_management_api/views.py
+++ user_management_api/views.py
@@ -16,17 +16,17 @@ from rest_framework.response import Response
 @permission_classes((AllowAny,))
 def login(request):
     print ('called login api')#zzz
-    username = request.data.get("username")
+    email = request.data.get("username")
     password = request.data.get("password")
     print ('username - '+username)#zzz
     print ('password - '+password)#zzz
     if username is None or password is None:
         return Response({'error': 'Please provide both username and password'},
                         status=HTTP_400_BAD_REQUEST)
-    user = authenticate(username=username, password=password)
+    user = authenticate(email=email, password=password)
     if not user:
         return Response({'error': 'Invalid Credentials'},
-                        status=HTTP_404_NOT_FOUND)
+                        status=HTTP_400_BAD_REQUEST)
     token, _ = Token.objects.get_or_create(user=user)
     print ("token.key - "+token.key)#zzz
     return Response({'token': token.key},
